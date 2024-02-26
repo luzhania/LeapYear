3a6036090d8affd3c99042cf60fad153a832e6e6
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-02-25 20:37:29 -0400

    The numbers divible by 100 but not by 400 shouldn't be considered a leap year

--- src/isLeap.js
+++ src/isLeap.js
@@ -1,9 +1,12 @@
 function isLeap(year){
-    if(year === 400){
+    if(year % 400 === 0){
         return true;
     }
     if(year % 4 === 0){
-        return true;
+        if(year % 100 !== 0)
+        {
+            return true;
+        }
     }
     return false;
 }
--- src/leapYear.spec.js
+++ src/leapYear.spec.js
@@ -13,6 +13,15 @@ describe("Leap Year", () => {
   it("The number 400 should be considered a leap year", () => {
     expect(isLeap(400)).toEqual(true);
   });
+  it("The numbers divible by 400 should be considered a leap year, such as 2000 ", () => {
+    expect(isLeap(2000)).toEqual(true);
+  });
+  it("The numbers divible by 100 but not by 400 shouldn't be considered a leap year, such as 1700 ", () => {
+    expect(isLeap(1700)).toEqual(false);
+  });
+  it("The numbers divible by 100 but not by 400 shouldn't be considered a leap year, such as 2100 ", () => {
+    expect(isLeap(2100)).toEqual(false);
+  });
 });
 
 
