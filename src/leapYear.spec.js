44246dc5c80d6f7c4339ba19a7969ab9f3fc8ca5
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-02-25 22:11:09 -0400

    Formats the tests

--- src/leapYear.spec.js
+++ src/leapYear.spec.js
@@ -19,22 +19,28 @@ describe("Leap Year", () => {
   it("The number 400 should be considered a leap year", () => {
     expect(isLeap(400)).toEqual(true);
   });
-  it("The numbers divible by 400 should be considered a leap year, such as 2000 ", () => {
+  it("The numbers divisible by 400 should be considered a leap year, such as 2000 ", () => {
     expect(isLeap(2000)).toEqual(true);
   });
-  it("The numbers divible by 100 but not by 400 shouldn't be considered a leap year, such as 1700 ", () => {
+  it("The numbers divisible by 100 but not by 400 shouldn't be considered a leap year, such as 1700 ", () => {
     expect(isLeap(1700)).toEqual(false);
   });
-  it("The numbers divible by 100 but not by 400 shouldn't be considered a leap year, such as 2100 ", () => {
+  it("The numbers divisible by 100 but not by 400 shouldn't be considered a leap year, such as 1800 ", () => {
+    expect(isLeap(1800)).toEqual(false);
+  });
+  it("The numbers divisible by 100 but not by 400 shouldn't be considered a leap year, such as 1900 ", () => {
+    expect(isLeap(1900)).toEqual(false);
+  });
+  it("The numbers divisible by 100 but not by 400 shouldn't be considered a leap year, such as 2100 ", () => {
     expect(isLeap(2100)).toEqual(false);
   });
-  it("The numbers divible by 4 but not by 100 should be considered a leap year, such as 2008 ", () => {
+  it("The numbers divisible by 4 but not by 100 should be considered a leap year, such as 2008 ", () => {
     expect(isLeap(2008)).toEqual(true);
   });
-  it("The numbers divible by 4 but not by 100 should be considered a leap year, such as 2012 ", () => {
+  it("The numbers divisible by 4 but not by 100 should be considered a leap year, such as 2012 ", () => {
     expect(isLeap(2012)).toEqual(true);
   });
-  it("The numbers divible by 4 but not by 100 should be considered a leap year, such as 2016 ", () => {
+  it("The numbers divisible by 4 but not by 100 should be considered a leap year, such as 2016 ", () => {
     expect(isLeap(2016)).toEqual(true);
   });
 });
